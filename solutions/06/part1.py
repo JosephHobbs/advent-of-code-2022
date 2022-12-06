a577da184d040bcac7d9c558a4b6b112d17a0859
joseph-hobbs <joseph.hobbs@53.com>
2022-12-06 13:30:58 -0500

    added day 6 solutions

---
+++ solutions/06/part1.py
@@ -0,0 +1,33 @@
+################################################################################
+# part1.py
+################################################################################
+
+with open('input.txt') as input_file:
+    input_data = input_file.read().strip()
+
+buffer = []
+letter_id = 0
+
+for letter in input_data:
+
+    # keep track of the current character id
+
+    letter_id += 1
+
+    # if our buffer is full, drop the first char.
+
+    if len(buffer) == 4:
+        buffer.pop(0)
+
+    # add the new char to the end of our buffer.
+
+    buffer.append(letter)
+
+    # if we have 4 unique, we have a start of packet.
+
+    if letter_id > 3 and len(buffer) == len(set(buffer)):
+        print(letter_id)
+
+################################################################################
+# END
+################################################################################
